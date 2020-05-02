a9d7779c5d8cb228873edff82330fcb51ef6128f
Emilija Krstonošić <ekrstonosic@gmail.com>
2020-05-02 18:48:21 +0200

    update

--- javaTest/Atlethics/src/competition/Athlete.java
+++ javaTest/Atlethics/src/competition/Athlete.java
@@ -1,21 +1,46 @@
 package competition;
 
-public abstract class Athlete {
-	
+import sport.AthleticsSport;
+
+public class Athlete {
+
 	private Gender gender;
 	private String fullName;
+	private AthleticsSport sport;
+	private double qualificationScore;
 
 	public Athlete(Gender gender, String fullName) {
 		this.gender = gender;
 		this.fullName = fullName;
 	}
-	
+
 	public Gender getGender() {
 		return this.gender;
 	}
-	
+
 	public String getFullName() {
 		return fullName;
 	}
-	
+
+	public void setSport(AthleticsSport sport) {
+		this.sport = sport;
+	}
+
+	public AthleticsSport getSport() {
+		return this.sport;
+	}
+
+	public double getQualificationScore() {
+		return qualificationScore;
+	}
+
+	public void setQualificationScore(double score) {
+		this.qualificationScore = score;
+	}
+
+	@Override
+	public String toString() {
+		return new StringBuilder().append(fullName).append(", compete ").append(sport).append(" with qualification score: ").append(qualificationScore)
+				.toString();
+	}
 }
--- javaTest/Atlethics/src/competition/Competition.java
+++ javaTest/Atlethics/src/competition/Competition.java
@@ -5,21 +5,21 @@ import sport.AAthletics;
 public abstract class Competition {
 
 	private AAthletics discipline;
-	
+
 	public Competition(AAthletics discipline) {
-		if(discipline == null) {
+		if (discipline == null) {
 			throw new NullPointerException("Discipline is mandatory!");
 		}
 		this.discipline = discipline;
 	}
-	
+
 	public AAthletics getDiscipline() {
 		return discipline;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "  Competition: " + discipline.toString();
 	}
-	
+
 }
