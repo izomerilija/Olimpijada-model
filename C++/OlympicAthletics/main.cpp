a3ff2b006f3c9c47a5818fd21112c6a6311f1207
Emilija Krstonošić <ekrstonosic@gmail.com>
2020-05-03 18:31:10 +0200

    test meeting

--- C++/OlympicAthletics/main.cpp
+++ C++/OlympicAthletics/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+#include "AAthletics.h"
+#include "Meeting.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Atletika" << endl;
+    Meeting tokyoMeeting;
+    tokyoMeeting.printDisciplines();
+
     return 0;
 }
